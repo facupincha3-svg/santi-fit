40cb832d56321e1d1ab97af2ce46d768e69cd578
Facu Pincha <facupincha3@gmail.com>
2026-07-01 10:21:57 -0300

    feat: monitorización de errores con Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://956874677171aa2ecf2e2e1e3f07f229@o4511660214583296.ingest.us.sentry.io/4511660220284928",
+  environment: "production",
+});
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
